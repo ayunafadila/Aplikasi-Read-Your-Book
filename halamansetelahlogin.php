adcaac1f17eaa0ac44860fdd31f54f0418d08860
Ayu Nafadila <ayu.nafadila@gmail.com>
2017-12-22 13:30:22 +0700

    Add files via upload

---
+++ halamansetelahlogin.php
@@ -0,0 +1,90 @@
+<html class="desktop">
+
+<head>
+	  <title>Read Your Book</title>
+  
+		<style type="text/css">
+			h3 {
+				text-indent: -9999px;
+				width: 940px;
+				height: 130px;
+				margin: 0px;}
+			nav ul {
+				margin: 0px;
+				padding: 0px 100px 100px 100px;}
+			section.courses {
+				float: left;
+				width: 659px;
+				border-right: 1px solid #eeeeee;
+				padding: 30px 30px;}
+		</style>
+
+<div id='masthead'>
+<div id='headline'>
+<img alt="Read Your Book" src="buku.png" />
+</div>
+<h1>Welcome to Read Your Book</h1>
+<h2>Reading is Better Together</h2>
+<li><a href="http://localhost">See Friends Who are on Read Your Book</a></li>
+
+<div id="browseBox">
+        <h2>Search and browse books</h2>
+
+        <div id="searchBox">
+          <div id='sitesearch'>
+<form id="headerSearchForm" action="/search" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="&#x2713;" /><div class='auto_complete_field_wrapper'>
+<input type="text" name="query" id="sitesearch_field" />
+<div id='sitesearch_autocomplete'></div>
+<a class="submitLink" href="#" onclick="$j(&#39;#headerSearchForm&#39;).submit(); return false;"><img width="16" title="Title / Author / ISBN" alt="search" src="https://s.gr-assets.com/assets/layout/magnifying_glass-a2d7514d50bcee1a0061f1ece7821750.png" /></a>
+</form>
+</div>
+
+</head>
+	<body>
+		<div class="wrapper">
+			<header>
+				<h3>Read Your Book</h3>
+				<nav>
+					<ul>
+						<a class="gr-hyperlink" href="/genres/art">Art</a><br>
+
+						<a class="gr-hyperlink" href="/genres/business">Business</a><br>
+
+						<a class="gr-hyperlink" href="/genres/children-s">Children</a><br>
+
+						<a class="gr-hyperlink" href="/genres/comics">Comics</a><br>
+
+						<a class="gr-hyperlink" href="/genres/cookbooks">Cook Books</a><br>
+
+						<a class="gr-hyperlink" href="/genres/fiction">Fiction</a><br>
+
+						<a class="gr-hyperlink" href="/genres/history">History</a><br>
+				  
+						<a class="gr-hyperlink" href="/genres/horror">Horror</a><br>
+						
+						<a class="gr-hyperlink" href="/genres/islamics">Islamic</a><br>
+
+						<a class="gr-hyperlink" href="/genres/motivation">Motivation</a><br>
+
+						<a class="gr-hyperlink" href="/genres/music">Music</a><br>
+
+						<a class="gr-hyperlink" href="/genres/nonfiction">Nonfiction<br>
+
+						<a class="gr-hyperlink" href="/genres/romance">Romance</a><br>
+
+						<a class="gr-hyperlink" href="/genres/science">Science</a><br>
+
+						<a class="gr-hyperlink" href="/genres/travel">Travel</a><br>
+					</ul>
+				</nav>
+			</header>
+			
+			</section>
+			<aside>
+				<section class="popular-recipes">
+					<h2>The Read Your Book Blog</h2>
+					<a href="http://localhost">7 Latest Islamic Book 2017 </a>
+					<a href="http://localhost">The Popular Book 2017</a>
+					<a href="http://localhost">The New Book 2017</a>
+				</section>
+				<section class="contact-details">
